fa307b0b575a5cd1fc5eb83526b58be544219554
DmytroAispidoraiSSS <dmitroslab@gmail.com>
2025-12-10 12:29:17 +0200

    laba7.py

--- laba7.py
+++ laba7.py
@@ -1,15 +1,22 @@
 import telebot
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-print("LOADED TOKEN =", os.getenv("TOKEN"))  
 
-bot = telebot.TeleBot(os.getenv("TOKEN"))
+def main():
+    load_dotenv()
 
-@bot.message_handler(func=lambda a: True)
-def echo(a):
-    bot.reply_to(a, a.text)
+    print("LOADED TOKEN =", os.getenv("TOKEN"))
 
-print("launch")
-bot.infinity_polling()
+    bot = telebot.TeleBot(os.getenv("TOKEN"))
+
+    @bot.message_handler(func=lambda a: True)
+    def echo(a):
+        bot.reply_to(a, a.text)
+
+    print("launch")
+    bot.infinity_polling()
+
+
+if __name__ == "__main__":
+    main()
